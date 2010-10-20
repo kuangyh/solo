47cca4057c2be8319f4daf12fe178138cbfe636f
yuheng <kuangyuheng@gmail.com>
2010-10-20 19:36:37 +0800

    sepreate invoke

---
+++ solo/invoke.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+Somewhat hard to explain
+"""
+import context
+
+class Invoke(object):
+    def __asop(src):
+        if isinstance(src, Invoke):
+            return ~src
+        raise TypeError, src
+    __conf_ctx = context.Context({'conf/asop' : __asop})
+    
+    def __init__(self, codesrc, params):
+        self.__codesrc = codesrc
+        self.__params = params
+        self.__func = eval('lambda _,p:' + codesrc)
+
+    def __invoke(self, datasrc):
+        return self.__func(datasrc, self.__params)
+
+    def __invert__(self):
+        return self.__invoke
+
+    @staticmethod
+    def _compile_param(idx, param):
+        import datatpl
+
+        # For simple data type, directly exit it
+        if type(param) in (int, float, basestring, bool):
+            return repr(param), ()
+
+        with Invoke.__conf_ctx:
+            tpl = datatpl.TPL(param)
+        if isinstance(tpl, datatpl.ConstTemplate):
+            return 'p[%d]' % (idx,), (param,)
+        else:
+            return '(p[%d](_))' % (idx,), (tpl,)
+
+    def __extend(self, codesrc, params = ()):
+        return type(self)(self.__codesrc + codesrc, self.__params + params)
+
+    def __getattr__(self, attrname):
+        return self.__extend('.' + attrname)
+
+    def __getitem__(self, key):
+        selector, data = Invoke._compile_param(len(self.__params), key)
+        return self.__extend('[%s]' % (selector,), data)
+
+    def __call__(self, *args, **kwargs):
+        idx = len(self.__params)
+        args_sel, args_data = Invoke._compile_param(idx, list(args))
+        kwargs_sel, kwargs_data = Invoke._compile_param(idx + 1, kwargs)
+        return self.__extend('(*%s,**%s)' % (args_sel, kwargs_sel),
+                             args_data + kwargs_data)
+
+IN = Invoke('_', ())
+IG = Invoke('globals()', ())
+import context
+IC = Invoke('p[0]()', (context.curr,))
+
+def I(start):
+    return Invoke('p[0]', (start,))
+
